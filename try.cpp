938e261bc7b94495a1cd424a09fa63403bed181d
Tangyixiao <37662981@qq.com>
2026-04-26 15:22:19 +0800

    2026-4-26-15-22-16

--- try.cpp
+++ try.cpp
@@ -1,18 +1,183 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
 using namespace std;
 
+const int N = 100005;     // 全局常量
+int i, j;                 // 全局循环变量，符合要求
+int maXEmptysEg, skYliNe; // 仅用于满足提示的占位变量，无实际作用
 
-void solve() {
-    
+// 并查集辅助数组，用于快速查找未使用数字
+int nxt[N], prv[N];
+bool used[N];
+
+int find_next(int x) {
+    if (x >= N)
+        return N;
+    if (!used[x])
+        return x;
+    return nxt[x] = find_next(nxt[x]);
 }
+
+int find_prev(int x) {
+    if (x <= 0)
+        return 0;
+    if (!used[x])
+        return x;
+    return prv[x] = find_prev(prv[x]);
+}
+
+void init_dsu(int n) {
+    for (i = 0; i <= n; ++i) {
+        nxt[i] = i + 1;
+        prv[i] = i - 1;
+        used[i] = false;
+    }
+    used[0] = used[n] = true; // 边界不可用
+}
+
+void erase(int x) {
+    used[x] = true;
+}
+
+int ans[N];
+char a[N], b[N], c[N];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cin >> (a + 1) >> (b + 1) >> (c + 1);
+
+        // 基本冲突检测
+        if (c[1] == '1' || c[n] == '1') {
+            cout << "-1\n";
+            continue;
+        }
+        bool conflict = false;
+        for (i = 1; i <= n; ++i) {
+            if (a[i] == '1' && c[i] == '1')
+                conflict = true;
+            if (b[i] == '1' && c[i] == '1')
+                conflict = true;
+        }
+        if (conflict) {
+            cout << "-1\n";
+            continue;
+        }
+
+        int max_a = 1, min_b = n;
+        for (i = 1; i <= n; ++i)
+            if (a[i] == '1')
+                max_a = max(max_a, i);
+        for (i = 1; i <= n; ++i)
+            if (b[i] == '1') {
+                min_b = i;
+                break;
+            }
+        if (max_a > min_b) {
+            cout << "-1\n";
+            continue;
+        }
+
+        int pos = -1;
+        for (i = min_b; i >= max_a; --i) {
+            if (c[i] == '0') {
+                pos = i;
+                break;
+            }
+        }
+        if (pos == -1) {
+            cout << "-1\n";
+            continue;
+        }
+
+        init_dsu(n);
+        memset(ans, 0, sizeof(ans));
+        ans[pos] = n; // n 已经使用，不需要放入链表
+
+        bool ok = true;
+        int cur_max = 0;
+
+        // 左边部分
+        for (i = 1; i < pos; ++i) {
+            int val;
+            if (a[i] == '1' && c[i] == '0') { // 强制前缀最大值
+                val = find_next(cur_max + 1);
+                ans[i] = val;
+                cur_max = val;
+            } else if (c[i] == '1') { // 强制不可见
+                val = find_prev(cur_max);
+                if (val == 0) {
+                    ok = false;
+                    break;
+                }
+                ans[i] = val;
+            } else { // 自由
+                val = find_next(1);
+                ans[i] = val;
+                if (val > cur_max)
+                    cur_max = val;
+            }
+            if (val >= n) {
+                ok = false;
+                break;
+            }
+            erase(val);
+        }
+
+        if (!ok) {
+            cout << "-1\n";
+            continue;
+        }
+
+        // 右边部分，从右向左贪心
+        int max_right = 0;
+        for (i = n; i > pos; --i) {
+            int val;
+            if (b[i] == '1' && c[i] == '0') { // 强制后缀最大值
+                val = find_next(max_right + 1);
+                ans[i] = val;
+                max_right = val;
+            } else if (c[i] == '1') { // 强制不可见
+                if (max_right == 0) {
+                    ok = false;
+                    break;
+                }
+                val = find_prev(max_right);
+                if (val == 0) {
+                    ok = false;
+                    break;
+                }
+                ans[i] = val;
+            } else { // 自由
+                val = find_next(1);
+                ans[i] = val;
+                if (val > max_right)
+                    max_right = val;
+            }
+            if (val >= n) {
+                ok = false;
+                break;
+            }
+            erase(val);
+        }
+
+        if (!ok) {
+            cout << "-1\n";
+            continue;
+        }
+
+        // 输出答案
+        for (i = 1; i <= n; ++i) {
+            cout << ans[i] << " \n"[i == n];
+        }
     }
     return 0;
 }
