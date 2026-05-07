69564177e6c94690b2c36c7245aea4e30a6ae452
Tangyixiao <37662981@qq.com>
2026-05-07 22:52:55 +0800

    2026-5-7-22-53-00

---
+++ CF1A Theatre Square.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a;
+int main() {
+    cin >> n >> m >> a;
+    cout << (long long)(ceil(n / (double)a) * ceil(m / (double)a));
+    return 0;
+}
