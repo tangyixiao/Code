01591447c7af57cc7dfeaecbcf33e68e89d58440
Tangyixiao <37662981@qq.com>
2026-06-16 19:01:11 +0800

    2026-6-16-19-01-08

---
+++ P14518 [NFLSPC #8] APLSPC token.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main() {
+    cout << "6c977d3c7c17b2ce8ddbf92a615187a2a559cc5c3044d14c250d5c7a6d6c4bd9 100" << "\n";
+    cout << R"(#define p main(){ch##ar*p
+p="#define p main(){ch##ar*p%cp=%c%s%c;1//1;print(end=p%%(10,34,p,34,10,10))%c#define p;__builtin_printf(p,10,34,p,34,10,10);}%cp";1//1;print(end=p%(10,34,p,34,10,10))
+#define p;__builtin_printf(p,10,34,p,34,10,10);}
+p)";
+    return 0;
+}
---
+++ P14518 [NFLSPC #8] APLSPC.cpp
@@ -0,0 +1,4 @@
+#define p main(){ch##ar*p
+p="#define p main(){ch##ar*p%cp=%c%s%c;1//1;print(end=p%%(10,34,p,34,10,10))%c#define p;__builtin_printf(p,10,34,p,34,10,10);}%cp";1//1;print(end=p%(10,34,p,34,10,10))
+#define p;__builtin_printf(p,10,34,p,34,10,10);}
+p
