a9a55dd01e20c3cac12a579fed76d8103c1112f2
tangyixiao <37662981@qq.com>
2026-06-30 09:03:04 +0800

    2026-6-30-09-03-42

--- POJ2311 Cutting Game.cpp
+++ POJ2311 Cutting Game.cpp
@@ -8,27 +8,30 @@
 // Powered by CP Editor (https://cpeditor.org)
 #include <iostream>
 using namespace std;
-
-bool g[201][201];
-
+int sg[201][201];
 int main() {
-    for (int w = 1; w <= 200; ++w) {
-        for (int h = 1; h <= 200; ++h) {
-            if (w == 1 || h == 1) {
-                g[w][h] = 1;
+    for (int w = 2; w <= 200; ++w)
+        for (int h = 2; h <= 200; ++h) {
+            if (w <= 3 && h <= 3)
                 continue;
+            bool v[500] = {0};
+            for (int i = 2; i <= w - 2; ++i) {
+                int t = sg[i][h] ^ sg[w - i][h];
+                if (t < 500)
+                    v[t] = 1;
             }
-            bool win = 0;
-            for (int i = 1; i < w && !win; ++i)
-                if (!g[i][h] && !g[w - i][h])
-                    win = 1;
-            for (int j = 1; j < h && !win; ++j)
-                if (!g[w][j] && !g[w][h - j])
-                    win = 1;
-            g[w][h] = win;
+            for (int j = 2; j <= h - 2; ++j) {
+                int t = sg[w][j] ^ sg[w][h - j];
+                if (t < 500)
+                    v[t] = 1;
+            }
+            int m = 0;
+            while (v[m])
+                ++m;
+            sg[w][h] = m;
         }
-    }
     int w, h;
     while (cin >> w >> h)
-        cout << (g[w][h] ? "WIN" : "LOSE") << '\n';
+        cout << (sg[w][h] ? "WIN" : "LOSE") << '\n';
+    return 0;
 }
