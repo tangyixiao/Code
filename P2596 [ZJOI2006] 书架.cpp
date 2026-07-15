0fc64b9f4d726ce7ecb2628a17eade8873ff5719
tangyixiao <37662981@qq.com>
2026-07-15 14:35:21 +0800

    2026-7-15-14-35-23

--- P2596 [ZJOI2006] 书架.cpp
+++ P2596 [ZJOI2006] 书架.cpp
@@ -1,104 +1,166 @@
-#include <bits/stdc++.h>
-#include <ext/rope> // 需要包含这个头文件
+#include <algorithm>
+#include <cstdio>
 using namespace std;
-using namespace __gnu_cxx; // rope在这个命名空间中
 
-const int MAXN = 80005;
-rope<int> rp;  // 使用rope存储书序列
-int pos[MAXN]; // 记录每本书在rope中的位置（下标）
-int rev[MAXN]; // 记录每个位置上书的编号
+const int N = 80005;
+int ch[N][2], fa[N], sz[N], val[N], pos[N], tot, root;
+int a[N];
+
+void pu(int x) { sz[x] = sz[ch[x][0]] + sz[ch[x][1]] + 1; }
+
+int bd(int l, int r) {
+    if (l > r)
+        return 0;
+    int m = (l + r) >> 1;
+    int x = ++tot;
+    val[x] = a[m];
+    pos[a[m]] = x;
+    int ls = bd(l, m - 1), rs = bd(m + 1, r);
+    ch[x][0] = ls;
+    if (ls)
+        fa[ls] = x;
+    ch[x][1] = rs;
+    if (rs)
+        fa[rs] = x;
+    pu(x);
+    return x;
+}
+
+void rot(int x) {
+    int y = fa[x], z = fa[y];
+    int k = (ch[y][1] == x);
+    if (z)
+        ch[z][ch[z][1] == y] = x;
+    fa[x] = z;
+    ch[y][k] = ch[x][k ^ 1];
+    if (ch[x][k ^ 1])
+        fa[ch[x][k ^ 1]] = y;
+    ch[x][k ^ 1] = y;
+    fa[y] = x;
+    pu(y);
+    pu(x);
+}
+
+void spl(int x, int g) {
+    while (fa[x] != g) {
+        int y = fa[x], z = fa[y];
+        if (z != g)
+            rot(((ch[y][1] == x) ^ (ch[z][1] == y)) ? x : y);
+        rot(x);
+    }
+    if (!g)
+        root = x;
+}
+
+int kth(int k) {
+    int u = root;
+    while (u) {
+        int l = sz[ch[u][0]];
+        if (k <= l)
+            u = ch[u][0];
+        else if (k == l + 1)
+            return u;
+        else
+            k -= l + 1, u = ch[u][1];
+    }
+    return 0;
+}
+
+int pre(int x) {
+    spl(x, 0);
+    int u = ch[x][0];
+    if (!u)
+        return 0;
+    while (ch[u][1])
+        u = ch[u][1];
+    return u;
+}
+
+int nxt(int x) {
+    spl(x, 0);
+    int u = ch[x][1];
+    if (!u)
+        return 0;
+    while (ch[u][0])
+        u = ch[u][0];
+    return u;
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
     int n, m;
-    cin >> n >> m;
-
-    // 初始化
-    for (int i = 1; i <= n; i++) {
-        int x;
-        cin >> x;
-        rp.push_back(x); // 将书加入rope
-    }
-
-    // 构建初始的位置映射
-    for (int i = 0; i < n; i++) {
-        int book = rp[i];
-        pos[book] = i;
-        rev[i] = book;
-    }
-
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; ++i)
+        scanf("%d", &a[i]);
+    root = bd(1, n);
+    char op[10];
+    int s, t;
     while (m--) {
-        string op;
-        cin >> op;
-
-        if (op == "Top") {
-            int s;
-            cin >> s;
-            int p = pos[s];
-            // 删除这本书
-            rp.erase(p, 1);
-            // 插入到最前面
-            rp.insert(0, s);
-            // 更新位置信息
-            pos[s] = 0;
-            // 更新rev数组（这里需要重新构建或者使用更高效的方法）
-            // 由于操作次数较多，这里使用直接遍历更新
-            for (int i = 0; i < rp.size(); i++) {
-                pos[rp[i]] = i;
+        scanf("%s%d", op, &s);
+        if (op[0] == 'T') {
+            int x = pos[s];
+            spl(x, 0);
+            int L = ch[x][0], R = ch[x][1];
+            if (!L)
+                continue;
+            ch[x][0] = 0;
+            if (!R) {
+                ch[x][1] = L;
+                fa[L] = x;
+                spl(L, 0);
+            } else {
+                int y = R;
+                while (ch[y][0])
+                    y = ch[y][0];
+                ch[y][0] = L;
+                fa[L] = y;
+                spl(y, 0);
             }
-        } else if (op == "Bottom") {
-            int s;
-            cin >> s;
-            int p = pos[s];
-            // 删除这本书
-            rp.erase(p, 1);
-            // 插入到最后面
-            rp.push_back(s);
-            // 更新位置信息
-            pos[s] = rp.size() - 1;
-            // 更新rev数组
-            for (int i = 0; i < rp.size(); i++) {
-                pos[rp[i]] = i;
+            pu(x);
+        } else if (op[0] == 'B') {
+            int x = pos[s];
+            spl(x, 0);
+            int L = ch[x][0], R = ch[x][1];
+            if (!R)
+                continue;
+            ch[x][1] = 0;
+            if (!L) {
+                ch[x][0] = R;
+                fa[R] = x;
+                spl(R, 0);
+            } else {
+                int y = L;
+                while (ch[y][1])
+                    y = ch[y][1];
+                ch[y][1] = R;
+                fa[R] = y;
+                spl(y, 0);
             }
-        } else if (op == "Insert") {
-            int s, t;
-            cin >> s >> t;
-            int p = pos[s];
-
+            pu(x);
+        } else if (op[0] == 'I') {
+            scanf("%d", &t);
             if (t == 0)
-                continue; // 位置不变
-
-            // 计算目标位置
-            int target = p + t;
-
-            // 确保target在有效范围内
-            if (target < 0)
-                target = 0;
-            if (target >= rp.size())
-                target = rp.size() - 1;
-
-            // 删除原位置的书
-            rp.erase(p, 1);
-            // 插入到新位置
-            rp.insert(target, s);
-
-            // 更新位置信息
-            for (int i = 0; i < rp.size(); i++) {
-                pos[rp[i]] = i;
+                continue;
+            int x = pos[s];
+            if (t == 1) {
+                int y = nxt(x);
+                swap(val[x], val[y]);
+                swap(pos[val[x]], pos[val[y]]);
+                spl(y, 0);
+            } else {
+                int y = pre(x);
+                swap(val[x], val[y]);
+                swap(pos[val[x]], pos[val[y]]);
+                spl(y, 0);
             }
-        } else if (op == "Ask") {
-            int s;
-            cin >> s;
-            cout << pos[s] << "\n"; // 位置从0开始计数
-        } else if (op == "Query") {
-            int s;
-            cin >> s;
-            s--; // 转换为0-based索引
-            cout << rp[s] << "\n";
+        } else if (op[0] == 'A') {
+            int x = pos[s];
+            spl(x, 0);
+            printf("%d\n", sz[ch[x][0]]);
+        } else if (op[0] == 'Q') {
+            int x = kth(s);
+            printf("%d\n", val[x]);
+            spl(x, 0);
         }
     }
-
     return 0;
 }
